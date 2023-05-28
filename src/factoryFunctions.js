64ede369d370ba3b9b95095cc51a53c86a76c753
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-05-28 13:17:58 +0300

    Created the gameboard function where you can add ships and a function that receiveAttacks, to see if a ship is hit or miss

--- src/factoryFunctions.js
+++ src/factoryFunctions.js
@@ -1,8 +1,13 @@
-function createShip(length, hits, sunk) {
+function createShip({ length, hits, sunk, row, col }) {
     return {
         length: length,
-        hits: hits,
-        sunk: sunk,
+        hits: hits || 0,
+        sunk: sunk || false,
+        id: Date.now(),
+        position: {
+            row: row,
+            column: col,
+        },
         hit() {
             this.hits++;
             this.isSunk();
@@ -15,4 +20,65 @@ function createShip(length, hits, sunk) {
     };
 }
 
-module.exports = createShip;
+function Gameboard() {
+    return {
+        locations: createGrid(),
+        ships: [],
+        placeShip(length, row, column, vertical) {
+            let position;
+            if (vertical === true) {
+                position = {
+                    row: [],
+                    column: [column],
+                };
+                for (let i = row; i < row + length; i++) {
+                    this.locations[i][column] = 1;
+                    position.row.push(i);
+                }
+            } else {
+                position = {
+                    row: [row],
+                    column: [],
+                };
+                for (let i = column; i < column + length; i++) {
+                    this.locations[row][i] = 1;
+                    position.column.push(i);
+                }
+            }
+            const ship = createShip({
+                length: length,
+                row: position.row,
+                col: position.column,
+            });
+            this.ships.push(ship);
+        },
+        receiveAttack(row, column) {
+            const shipHit = this.ships.some((ship) => {
+                if (
+                    ship.position.row.includes(row) &&
+                    ship.position.column.includes(column)
+                ) {
+                    ship.hit();
+                    return true;
+                }
+                return false;
+            });
+            if (shipHit === false) {
+                this.locations[row][column] = "x";
+            }
+        },
+    };
+}
+
+function createGrid() {
+    let a = [];
+    for (let i = 0; i < 10; i++) {
+        a[i] = [];
+        for (let j = 0; j < 10; j++) {
+            a[i][j] = 0;
+        }
+    }
+    return a;
+}
+
+module.exports = { createShip };
--- src/factoryFunctions.test.js
+++ src/factoryFunctions.test.js
@@ -1,10 +1,11 @@
-const createShip = require("./index");
+const imported = require("./factoryFunctions");
 
 test("creates a new ship of length 4 and with 0 hits", () => {
-    expect(createShip(4, 0, false)).toMatchObject({
+    expect(imported.createShip(4, 0, false, true)).toMatchObject({
         length: 4,
         hits: 0,
         sunk: false,
+        vertical: true,
         hit: expect.any(Function),
         isSunk: expect.any(Function),
     });
